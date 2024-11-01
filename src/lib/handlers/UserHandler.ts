e9a64e6f989932f2ef2b261b5bb41a652c4eff12
herc <herculeshorse@tuta.io>
2024-11-01 20:46:49 +0200

    fix

--- src/lib/handlers/UserHandler.ts
+++ src/lib/handlers/UserHandler.ts
@@ -5,16 +5,9 @@ import { hashedString } from "../helpers/generators";
 
 export class UserHandler {
   static async getAuthentificatedUser(context: APIContext) {
-    const data = await cacheResult(
-      `user-${hashedString(JSON.stringify(context.request.headers))}`,
-      500,
-      async () => {
-        const authentificatedUser = await auth.api.getSession({
-          headers: context.request.headers,
-        });
-        return authentificatedUser?.user;
-      }
-    );
-    return data;
+    const authentificatedUser = await auth.api.getSession({
+      headers: context.request.headers,
+    });
+    return authentificatedUser?.user;
   }
 }
