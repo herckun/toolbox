3d6d5d84ee4a9075878fb9a1db984da8e5cd3da7
herc <herculeshorse@tuta.io>
2024-11-01 20:20:16 +0200

    fix

--- src/lib/utils/react-auth-client.ts
+++ src/lib/utils/react-auth-client.ts
@@ -1,5 +1,4 @@
 import { createAuthClient } from "better-auth/react";
-import { APP_URL } from "../../consts/paths";
 export const authClient = createAuthClient({
-  baseURL: `${APP_URL}${import.meta.env.PUBLIC_BETTER_AUTH_URL}`,
+  baseURL: import.meta.env.PUBLIC_BETTER_AUTH_URL,
 });
